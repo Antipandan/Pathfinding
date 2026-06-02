d183fc248437719433421e6b4d2dd0ef4660af4b
Jacob <jacob@blota.se>
2026-06-02 10:13:38 +0200

    setup some stuff

--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -6,18 +6,24 @@ using UnityEngine.Tilemaps;
 using Utility;
 using Screen = UnityEngine.Device.Screen;
 
-[RequireComponent(typeof(Tilemap), typeof(TilemapRenderer), typeof(Grid))]
 public class GenerateMap : MonoBehaviour, IGenerateMap
 {
     [SerializeField] private ushort rows;
     [SerializeField] private ushort columns;
-    
-    private TilemapRenderer tilemapRenderer;
-    private Grid tilemapGrid;
-    private Tilemap tilemap;
-    
+    [Tooltip("Measured in pixels")]
+    [SerializeField] private ushort mapIndentationX;
+    [Tooltip("Measured in pixels")]
+    [SerializeField] private ushort mapIndentationY;
+
+    private int coefficientScreenToWorldPosition;
     private int screenHeight;
     private int screenWidth;
+    
+    private int mapWidthWorldPos;
+    private int mapHeightWorldPos;
+    private int nrRows;
+    private int nrColumns;
+    
     private Square[,] squares;
 
     public Square[,] GetSquares => squares;
@@ -26,7 +32,6 @@ public class GenerateMap : MonoBehaviour, IGenerateMap
     {
         GetAllNecessaryStuff();
         GenerateGrid();
-        FillRectangle(Vector3Int.zero);
     }
     
     
@@ -38,10 +43,9 @@ public class GenerateMap : MonoBehaviour, IGenerateMap
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private void GetAllNecessaryStuff()
     {
-        tilemapGrid = GetComponentInChildren<Grid>();
-        tilemap = GetComponent<Tilemap>();
-        tilemapRenderer = GetComponent<TilemapRenderer>();
         GetScreenHeightAndWidth();
+        GetMapWidthHeight();
+        GetNrRowsColumns();
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -50,27 +54,19 @@ public class GenerateMap : MonoBehaviour, IGenerateMap
         screenWidth = Screen.width;
         screenHeight = Screen.height;
     }
-    
-    
-    
-    private void FillRectangle(Vector3Int origin)
-    {
-        FillTile(new Vector3Int(0, 0, 0), Color.red);
-        for (int x = 0; x < rows; x++)
-        {
-            for (int y = 0; y < columns; y++)
-            {
-                Vector3Int pos = new Vector3Int(origin.x + x, origin.y + y, origin.z);
 
-            }
-        }
-        tilemap.RefreshAllTiles();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void GetMapWidthHeight()
+    {
+        mapWidthWorldPos = ((screenWidth - mapIndentationX) - (0 +  mapIndentationX)) * coefficientScreenToWorldPosition;
+        mapHeightWorldPos = ((screenHeight - mapIndentationY) - (0 + mapIndentationY)) * coefficientScreenToWorldPosition;
+        coefficientScreenToWorldPosition = (int) (screenWidth / 10f);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void FillTile(Vector3Int position, Color color)
+    private void GetNrRowsColumns()
     {
-        tilemap.SetColor(position, color);
+        nrRows = mapWidthWorldPos / rows;
+        nrColumns = mapHeightWorldPos / columns;
     }
-    
 }
